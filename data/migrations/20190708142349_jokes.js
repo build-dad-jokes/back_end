7a002549ad9fd3a1e825b79fd93d3f95a5c60541
Brennon Nuckols <brennuck@gmail.com>
2019-07-08 15:16:52 -0600

    Added seeds and fixed up some tables

--- data/migrations/20190708142349_jokes.js
+++ data/migrations/20190708142349_jokes.js
@@ -1,7 +1,12 @@
 exports.up = function(knex, Promise) {
     return knex.shema.createTable("jokes", tbl => {
         tbl.increments();
-    })
+        tbl
+          .string('joke', 128)
+          .notNullable()
+          .unique();
+        tbl.string('answer', 128).notNullable();
+    });
 }
 
 exports.down = function(knex, Promise) {
