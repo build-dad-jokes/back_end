4a7b531767b0e2bc24250fde16e86db67e8d05f8
Oliver-Strange <preston.burton@gmail.com>
2019-07-08 12:55:22 -0700

    creates authentication, jwt service, and role check. adds folders for user routes/model and jokes routes/model

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const configMiddleware = require("./middleware");
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/users-router");
+const jokesRouter = require("../jokes/jokes-router");
+
+const server = express();
+
+configMiddleware(server);
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/jokes", jokesRouter);
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+module.exports = server;
---
+++ config/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || "Back end has all the secrets"
+};
