61be38c674871fd8cb89f46168afab4ad182288f
Brennon Nuckols <brennuck@gmail.com>
2019-07-09 10:43:03 -0600

    jokes-model done

--- jokes/jokes-model.js
+++ jokes/jokes-model.js
@@ -1,1 +1,50 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
+const jokeHelper = require("./joke-helper.js");
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove,
+  filter
+};
+
+function find() {
+  let loadJokes = db("jokes");
+
+  return loadJokes.then(jokes => {
+    return jokes.map(joke => jokeHelper.convertBoolean(joke));
+  });
+}
+
+function findById(id) {
+  return db("jokes")
+    .where({ id })
+    .first();
+}
+
+function filter() {
+  let loadJokes = db("jokes").where({ public: true });
+  return loadJokes.then(jokes => {
+    return jokes.map(joke => jokeHelper.convertBoolean(joke));
+  });
+}
+
+async function add(joke) {
+  const [id] = await db("jokes").insert(joke);
+
+  return findById(id);
+}
+
+function update(id, changes) {
+  return db("jokes")
+    .where({ id })
+    .update(changes, "*");
+}
+
+function remove(id) {
+  return db("jokes")
+    .where({ id })
+    .del();
+}
--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -1,63 +1,66 @@
-const express = require('express');
-const jokesDB = require('./jokes-model.js');
+const express = require("express");
+const jokesDB = require("./jokes-model.js");
 const router = express.Router();
-const { restricted } = require('../auth/restricted-middleware.js');
-const { checkPublic, validateBody, validateId } = require('./jokes-middleware.js');
+const { restricted } = require("../auth/restricted-middleware.js");
+const {
+  checkPublic,
+  validateBody,
+  validateId
+} = require("./jokes-middleware.js");
 
-router.get('/', checkPublic, restricted, async (req, res) => {
-    try {
-        const jokes = await jokesDB.find();
+router.get("/", checkPublic, restricted, async (req, res) => {
+  try {
+    const jokes = await jokesDB.find();
 
-        res.status(200).json(jokes);
-    } catch(err) {
-        res.status(500).json({ success: false, err });
-    };
+    res.status(200).json(jokes);
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
 });
 
-router.get('/:id', restricted, validateId, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const joke = await jokesDB.findById(id);
+router.get("/:id", restricted, validateId, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const joke = await jokesDB.findById(id);
 
-        res.status(200).json(joke);
-    } catch(err) {
-        res.status(500).json({ success: false, err });
-    };
+    res.status(200).json(joke);
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
 });
 
-router.post('/', restricted, validateBody, async (req, res) => {
-    try {
-        const newJoke = await jokesDB.add(req.body);
+router.post("/", restricted, validateBody, async (req, res) => {
+  try {
+    const newJoke = await jokesDB.add(req.body);
 
-        res.status(201).json(newJoke);
-    } catch(err) {
-        res.status(500).json({ success: false, err });
-    };
+    res.status(201).json(newJoke);
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
 });
 
-router.put('/:id', restricted, validateId, validateBody, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updateJoke = await jokesDB.update(id, req.body);
+router.put("/:id", restricted, validateId, validateBody, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateJoke = await jokesDB.update(id, req.body);
 
-        updateJoke
-        ? res.status(200).json({ message: "Updated Joke Successfully" })
-        : res.status(404).json({ message: 'Joke Missing' })
-    }   catch(err) {
-        res.status(500).json({ message: "Error!!" })
-    };
+    updateJoke
+      ? res.status(200).json({ message: "Updated Joke Successfully" })
+      : res.status(404).json({ message: "Joke Missing" });
+  } catch (err) {
+    res.status(500).json({ message: "Error!!" });
+  }
 });
 
-router.delete('/:id', restricted, validateId, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const success = await jokesDB.remove(id);
+router.delete("/:id", restricted, validateId, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const success = await jokesDB.remove(id);
 
-        success
-        ? res.status(204).json.end() : res.status(404).end();
-    } catch(err) {
-        res.status(500).json({ success: false, err });
-    };
+    success ? res.status(204).json.end() : res.status(404).end();
+  } catch (err) {
+    res.status(500).json({ success: false, err });
+  }
 });
 
 module.exports = router;
